12e1b620da89ccb411334a9293e85900bc4ddea2
jaymccormick <jaymccormi@gmail.com>
2018-05-05 09:22:00 -0400

    capitalizing li on mouseover

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let addInput = document.querySelector('input.add-item');
 const addButton = document.querySelector('button.add-item');
 const removeButton = document.querySelector('button.remove-item');
 const ul = document.querySelector('ul');
+const liList = document.getElementsByTagName('li');
 
 addButton.addEventListener('click', () => {
   const text = addInput.value;
@@ -12,7 +13,12 @@ addButton.addEventListener('click', () => {
 });
 
 removeButton.addEventListener('click', () => {
-  const liList = document.getElementsByTagName('li');
   const lastLI = liList[liList.length - 1];
   ul.removeChild(lastLI);
 });
+
+for (let i = 0; i < liList.length; i++){
+  liList[i].addEventListener('mouseover', () => {
+    liList[i].style.textTransform = 'uppercase';
+  });
+}
